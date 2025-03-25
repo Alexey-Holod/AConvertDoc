5ff8405ee009227ba7c9b049ccfef63fe62eac78
Alexey-Holod <Alexey95holod@yandex.ru>
2025-03-25 16:17:59 +0500

    Сделал отдачу файла пользователю

--- Convert/views.py
+++ Convert/views.py
@@ -1,7 +1,7 @@
 import os
 from time import time
 from django.core.files.storage import FileSystemStorage
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, FileResponse
 from django.shortcuts import render
 from .forms import UploadFileForm
 from pdf2docx import Converter
@@ -13,11 +13,8 @@ def upload_file(request):
         uploaded_file = request.FILES['file']
         fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'uploads'))
         filename = fs.save(uploaded_file.name, uploaded_file)
-        # file_url = fs.url(filename)
-        # file_url = os.path.join(settings.MEDIA_ROOT, filename)
-        # give_file_url = os.path.join(settings.MEDIA_ROOT, str(time()) + '.docx')
         file_url = os.path.join(settings.MEDIA_ROOT, 'uploads/', filename)
-        give_file_url = os.path.join(settings.MEDIA_ROOT, 'converted/', str(time()), '.docx')
+        # give_file_url = os.path.join(settings.MEDIA_ROOT, 'converted/', str(time()), '.docx')
 
         print('filename -', filename)
         print('file_url -', file_url)
@@ -25,24 +22,31 @@ def upload_file(request):
         if not os.path.exists(file_url):
             print("Файл не найден:", file_url)
             return HttpResponse("Ошибка: файл не найден", status=400)
-        else:
-            print('*******File found*******')
-
-        # with open(file_url, "rb") as pdf_file:
-        #     cv = Converter(pdf_file)
-        #     cv.convert(give_file_url)
-        #     cv.close()
 
         # Задаём путь для сохранения результата в /media/converted/
         converted_dir = os.path.join(settings.MEDIA_ROOT, 'converted')
         os.makedirs(converted_dir, exist_ok=True)
-        give_file_url = os.path.join(converted_dir, str(int(time())) + '.docx')
+
+        time_file = str(time())
+
+        give_file_url = os.path.join(converted_dir, time_file + '.docx')
+
+        # Путь для скачивания
+        url_download = os.path.join(settings.MEDIA_URL, 'converted/', time_file + '.docx')
 
         cv = Converter(file_url)
         cv.convert(str(give_file_url))
         cv.close()
-        
-        return HttpResponseRedirect('/')
+
+        # Проверяем, существует ли файл
+        if not os.path.exists(give_file_url):
+            return HttpResponse("Файл не найден", status=404)
+        else:
+            # Создаём ссылку для скачивания
+            download_url = f"/download/{filename}"
+            print('*******Download*******', url_download)
+
+        return render(request, "base.html", {"download_url": url_download})
     else:
         form = UploadFileForm()
     return render(request, "base.html", {"form": form})
